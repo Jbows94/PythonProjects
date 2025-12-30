774af46214abb6f154aaf76149b7602e5e41e7f7
Jbows94 <jusbowser@hotmail.com>
2025-12-29 21:50:50 -0600

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -2,18 +2,25 @@
 #Game picks random number between 1&10
 import random
 num = random.randint(1,10)
-
+guess = None
+guess_count = 0
 #until guess matches num they will be asked to reenter guess
-try:
-    while guess != True
+while guess != num:
+    try:
         guess= int(input('Enter number between 1 & 10: '))
+        guess_count += 1
+        if guess < 1 or guess > 10:
+            print('Number out of range! Try again.')
+            continue
         if guess < num: 
             print('Higher')
-            print('Enter new guess' + guess)
+            print(guess)
         elif guess > num: 
             print('Lower')
             print (guess)
-    if guess == num: 
-        print('Correct! You Win!')
-except TypeError:
-    print('Error. Enter a number 1-10')
+        if guess == num: 
+            print('Correct! You Win!')
+            break
+    except:
+        raise ValueError('Not a number')
+print('Total guesses: ' + str(guess_count))        
