f411b360efa71d112e98b278f98bbbe82bac8354
Jbows94 <jusbowser@hotmail.com>
2025-12-29 21:23:30 -0600

    Update BudgetTracker.py

--- BudgetTracker.py
+++ BudgetTracker.py
@@ -1,31 +1,36 @@
 #function for difference 
-def diff(a, b):
-    return a - b
+import functions
 
 #declare monthly income
-inc = int(input('Enter your monthly income: ')
+inc = int(input('Enter your monthly income: '))
 #declare dollar amount of each bill
-#bills are stored in a list until user cancels
+#bills are stored in a list until user cancels  
 bills = []
-while True: 
-    bill = int(input('Enter bills or q to cancel: ')
-    if bill.lower() == q
-        break
-    bills.append(bill)
+try: 
+    while True:
+        user_input = input('Enter bills or q to cancel: ')
+        if user_input.lower() == 'q':
+            break 
+        bill = int(user_input)    
+        bills.append(bill)
+except ValueError:
+    print('Error. Enter bill amount')
+    
+   
 #Add up all bills in list to find total amount
 total_bills = sum(bills)  
-print('Your total bills are $' total_bills)
+print('Your total bills are $' + str(total_bills))
 #find budget by calculating diff of income vs bills  
-budget = diff(inc, total_bills)
-print('Your budget is $' budget)
+budget = functions.diff(inc, total_bills)
+print('Your budget is $' + str(budget))
 #user enters total of extra spending 
-spending = int(input('Enter how much extra you have spent this month: ')
+spending = int(input('Enter how much extra you have spent this month: '))
 #find total expenses for month
+exp_total = (functions.diff(budget, spending))
 #informs if over budget or on track
-total_exp = diff(budget, spending)
-if total_exp < 0:
-    print ('Total Expenses: $' + str(total_exp))
+if exp_total < 0:
+    print ('Total Expenses: $' + str(exp_total))
     print('You are over budget')
 else:    
-    print ('Total Expenses: $' + str(total_exp))
+    print ('Total Expenses: $' + str(exp_total))
     print('You are on track')
