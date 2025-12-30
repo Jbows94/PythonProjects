7880e11fb9daa1d67883520de1f3cf53d5452a77
Jbows94 <jusbowser@hotmail.com>
2025-12-30 13:44:53 -0600

    Update SimpleCalculator.py

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,20 +1,12 @@
 #define functions
-def add(a, b):
-    return a + b
-def diff(a, b):
-    return a - b
-def multiply(a, b):
-    return a * b
-def div(a, b):
-    return a / b
-
+import functions
 
 #declare x and y values
 x= int(input('X= ? '))
 y= int(input('Y= ? '))
 
 #print all four operations
-print('Sum: ', add(x, y))
-print('Difference: ', diff(x, y))
-print('Multiplied: ', multiply(x, y))
-print('Divided: ', div(x, y))
+print('Sum: ', functions.add(x, y))
+print('Difference: ', functions.diff(x, y))
+print('Multiplied: ', functions.multiply(x, y))
+print('Divided: ', functions.div(x, y))
